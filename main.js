00ebd4ce038c45a9a934e1b7410ce63977a8b255
Matthew McMorries <matthew.mcmorries@gmail.com>
2022-03-19 11:07:00 -0500

    week2 NSS

---
+++ main.js
@@ -0,0 +1,15 @@
+const createWoodBlock = () => {
+    const woodObject = {
+        type: "wood block",
+        length: 10,
+        material: "pine",
+        purpose: "flute"
+    }
+    return woodObject
+}
+
+const createBeautifulCarving = (woodObject) => {
+    console.log(`The ${woodBlock.length}-inch, ${woodBlock.material} woodblock was carved into a wooden ${woodBlock.purpose}`)
+
+} 
+
